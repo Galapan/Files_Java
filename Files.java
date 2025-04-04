bcb8defafd2a0b2c53b1ee52ecf3fdae44897c5f
Bastian Alessandro Gallegos <bastian4le@gmail.com>
2025-04-04 13:51:51 -0600

    Read n Write Arrays

--- Files.java
+++ Files.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 
 public class Files {
@@ -25,12 +27,12 @@ public class Files {
         return numLines; 
     }
 
-    public int[] fileToIntArray(String fileName){
+    public String[] fileToIntArray(String fileName){
         File file; //Point a physical file of dd
         FileReader reader; //Key with permission (only read)
         BufferedReader bufer; //Recover the data from the file
         String linea; //Line of the file
-        int[] array = null; //Array of numbers
+        String[] array = null; //Array of numbers
         int i=0; //Index of the array
         int t; //size of the array
         try {
@@ -39,13 +41,13 @@ public class Files {
         } finally {
             try {
                 t = countFileLines(fileName);
-                array = new int[t];
+                array = new String[t];
                 file = new File("C:\\Archivos\\" + fileName);
                 reader = new FileReader(file); //Open the file only read
                 bufer = new BufferedReader(reader); //Count the lines of the file
                 // Read each line of the file n put it in the array
                 while ( (linea = bufer.readLine()) != null) {
-                    array[i] = Integer.parseInt(linea); //Convert the string to int
+                    array[i] = linea; //Convert the string to int
                     i++;
                 }
                 reader.close();
@@ -54,5 +56,19 @@ public class Files {
             }
         }
         return array;
+    }
+    //Create a file with info of one array
+    public void writeIntArrayToFile(String fileName, int[] array){
+        FileWriter file; //Key with permission (only write)
+        PrintWriter writer; //Write the data in the file
+        try {
+            file = new FileWriter("C:\\Archivos\\" + fileName);
+            writer = new PrintWriter(file);
+            for (int unDato : array) 
+                writer.println(unDato); //Write the data in the file
+            file.close(); //Close the file
+        } catch (Exception e) {
+            System.out.println("Error al crear el archivo: " + e.toString());
+        }
     }
 }
--- TestingFiles.java
+++ TestingFiles.java
@@ -1,31 +1,47 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-
 public class TestingFiles {
 
-    public static void printArray(int[] array){
-        for(int i=0; i<array.length; i++){
-            System.out.println(array[i]);
+    public static void printArray(int[] numeros) {
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
         }
-    }    
+    }
 
+    public static void printArray(String[] songs) {
+        for (int i = 0; i < songs.length; i++) {
+            System.out.println(songs[i]);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        Files files  = new Files();
+        Files files = new Files();
         BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
         String fileName;
-        int[] numeros;
+        String[] songs;
+        String[] numeros;
+        int[] numeros2 = {27, 29, 33, 68, 10, 1, 15, 87, 95};
 
         System.out.println("----------------------------------------");
-        System.out.println("Program that read files");
+        System.out.println("Program that reads numbers from a file");
         System.out.println("Write the name of the file: ");
         fileName = bufer.readLine();
         numeros = files.fileToIntArray(fileName);
-        System.out.println("Content of the array: ");
+        System.out.println("Content of the array (numbers): ");
         printArray(numeros);
-    }        
+
+        System.out.println("----------------------------------------");
+        System.out.println("Program that reads text from a file");
+        System.out.println("Write the name of the file: ");
+        fileName = bufer.readLine();
+        songs = files.fileToIntArray(fileName); // Leer texto
+        System.out.println("Content of the array (text): ");
+        printArray(songs);
+
+        System.out.println("Escribe el nombre del archivo de números para guardar: ");
+        fileName = bufer.readLine();
+        files.writeIntArrayToFile(fileName, numeros2); // Guardar números en un archivo
+    }
 }
