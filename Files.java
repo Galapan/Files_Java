be27268804b38a655a5b83bdfa66fe87dfae1ac4
Bastian Alessandro Gallegos <bastian4le@gmail.com>
2025-04-04 12:53:48 -0600

    Arrays

--- Files.java
+++ Files.java
@@ -5,6 +5,26 @@ import java.io.FileReader;
 
 public class Files {
 
+    int countFileLines(String fileNames){
+        File file;
+        FileReader reader;
+        BufferedReader bufer;
+        int numLines = 0;
+
+        try{
+            file = new File("C:\\Archivos\\" + fileNames);
+            reader = new FileReader(file);
+            bufer = new BufferedReader(reader); //Count the lines of the file
+            while ((bufer.readLine()) != null) {
+                numLines++;
+            }
+            reader.close();
+        }catch ( Exception e){
+            System.out.println("Error al leer el archivo: " + e.toString());
+        }
+        return numLines; 
+    }
+
     public int[] fileToIntArray(String fileName){
         File file; //Point a physical file of dd
         FileReader reader; //Key with permission (only read)
@@ -20,12 +40,19 @@ public class Files {
             try {
                 t = countFileLines(fileName);
                 array = new int[t];
-                file = new File("C:\\archivos\\" + fileName);
-                reader = new FileReader(file);
+                file = new File("C:\\Archivos\\" + fileName);
+                reader = new FileReader(file); //Open the file only read
                 bufer = new BufferedReader(reader); //Count the lines of the file
-            } catch (Exception e) {
+                // Read each line of the file n put it in the array
+                while ( (linea = bufer.readLine()) != null) {
+                    array[i] = Integer.parseInt(linea); //Convert the string to int
+                    i++;
+                }
+                reader.close();
+            } catch ( Exception e) {
                 System.out.println("Error al leer el archivo: " + e.toString());
             }
         }
+        return array;
     }
 }
--- TestingFiles.java
+++ TestingFiles.java
@@ -1,6 +1,31 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
 public class TestingFiles {
-    public static void main(String[] args) {
-        
+
+    public static void printArray(int[] array){
+        for(int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+    }    
+
+
+    public static void main(String[] args) throws IOException {
+        Files files  = new Files();
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String fileName;
+        int[] numeros;
+
+        System.out.println("----------------------------------------");
+        System.out.println("Program that read files");
+        System.out.println("Write the name of the file: ");
+        fileName = bufer.readLine();
+        numeros = files.fileToIntArray(fileName);
+        System.out.println("Content of the array: ");
+        printArray(numeros);
     }        
 }
